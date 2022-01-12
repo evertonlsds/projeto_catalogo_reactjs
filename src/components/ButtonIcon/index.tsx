bd4d7057d4eef481c4e8014f09ba1f7b0ce88ffa
Everton santos <everton0102@gmail.com>
2022-01-12 14:20:16 -0300

    finalizado o layout x-small

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,9 @@
+import './styles.css'
+
+export default function ButtonIcon(){
+    return(
+        <button className="btn btn-primary btn-icon">
+            <h6>Inicie agora a sua busca</h6>
+        </button>
+    )
+}
