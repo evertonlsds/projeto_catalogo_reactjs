c34edaa8e537abd9f2f8c34d6c9cee320440ea19
Everton santos <everton0102@gmail.com>
2022-01-12 10:41:47 -0300

    add conteudos na pagina home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,20 @@
+import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
 import Navbar from 'components/Navbar';
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <h1>home</h1>
+      <div className="home-container">
+        <div className="home-card">
+          <div className="home-content-container">
+            <h1>Conheça o melhor catálogo de produtos</h1>
+          </div>
+          <div className="home-image-container">
+            <MainImage />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
