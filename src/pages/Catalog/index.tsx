5e3800d8fd6350cbac5d480d38340ab4982595df
Everton santos <everton0102@gmail.com>
2022-01-15 11:08:00 -0300

    iniciando pagina de catalago

---
+++ src/pages/Catalog/index.tsx
@@ -0,0 +1,13 @@
+import Navbar from 'components/Navbar';
+import './styles.css';
+
+export default function Catalog() {
+  return (
+    <>
+      <Navbar />
+      <div className="container my-4">
+        <h1>tela catalog</h1>
+      </div>
+    </>
+  );
+}
