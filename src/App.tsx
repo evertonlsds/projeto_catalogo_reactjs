7a44343efaa523f767c370b9f15ef0667e822448
Everton santos <everton0102@gmail.com>
2022-01-18 16:18:45 -0300

    criada rota pagina product details

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from 'pages/Home';
 import Catalog from 'pages/Catalog';
 import Admin from 'pages/Admin';
+import ProductDetails from 'pages/ProductDetails';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/catalog" element={<Catalog />} />
       <Route path="/admin" element={<Admin />} />
+      <Route path="/catalog:productId" element={<ProductDetails />} />
     </Routes>
   );
 }
---
+++ src/pages/ProductDetails/index.tsx
@@ -0,0 +1,5 @@
+export default function ProductDetails(){
+    return(
+        <div><h1>pagina product details</h1></div>
+    )
+}
