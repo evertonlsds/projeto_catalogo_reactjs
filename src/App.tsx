29bb3acf79b8e19543be8e1303c6d588f8930193
Everton santos <everton0102@gmail.com>
2022-01-18 17:19:35 -0300

    rota com parametro

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/catalog" element={<Catalog />} />
       <Route path="/admin" element={<Admin />} />
-      <Route path="/catalog:productId" element={<ProductDetails />} />
+      <Route path="/catalog/:productId" element={<ProductDetails />} />
     </Routes>
   );
 }
